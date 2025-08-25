c9ec260f112a46abe049fa1d16cb93e7969dd763
kumar6646 <sai.penusoft@gmail.com>
2025-08-25 15:38:29 +0530

    Add project files.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a numer");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter a number");
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a / b;
+                Console.WriteLine("value of c is " + c);
+            }
+            catch (DivideByZeroException D)
+            {
+                Console.WriteLine("Please enter b value other than zero");
+            }
+            catch (FormatException F)
+            {
+                Console.WriteLine("Please enter only numbers");
+            }
+            catch (ArgumentOutOfRangeException E)
+            {
+                Console.WriteLine("Please enter with in the integer range");
+            }
+            finally
+            {
+                Console.WriteLine("Irrespective of error code will execute");
+            }
+        }
+    }
+}
